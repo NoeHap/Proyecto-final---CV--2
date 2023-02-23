a4a6d684fcdcbec0378982381f98229df2fd4f08
Noemí Haponiuk <nohapk@gmail.com>
2023-02-23 09:17:39 -0300

    Mi proyecto final - Haponiuk

---
+++ js/Function.js
@@ -0,0 +1,20 @@
+
+document.getElementById("boton1").onclick = function mostrar(params) {
+
+    document.getElementById("demo1").style.display = "block";
+    
+}
+document.getElementById("boton2").onclick = function mostrar(params) {
+
+    document.getElementById("demo2").style.display = "block";
+        
+    }
+document.getElementById("boton3").onclick = function mostrar(params) {
+
+     document.getElementById("demo3").style.display = "block";
+            
+        }
+document.getElementById("boton4").onclick = function mostrar(params) {
+
+    document.getElementById("demo4").style.display = "block";
+        }
